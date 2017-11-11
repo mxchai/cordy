cf1fca7070f91e7163366c712f217e6b82753e97
MX Chai <mingxuan@live.com.sg>
2017-11-11 14:17:30 +0800

    Handle ArrayExpression, slight refactor

--- cordy.js
+++ cordy.js
@@ -1,19 +1,61 @@
-//////////// Helper functions ////////////
-function getTaint() {
-  return 0;
-}
-
 //////////// cordy Babel plugin ////////////
 module.exports = function(babel) {
   var t = babel.types;
+
+  function handleExpression(expression) {
+    return 0;
+  }
+
+
+  /**
+   * chainBinaryOr - Used in ArrayExpression to generate the
+   * taint expression
+   *
+   * @param  {type} arrElements ArrayExpression.init.elements
+   * @return {type}             description
+   */
+  function chainBinaryOr(arrElements) {
+    if (arrElements.length == 1) {
+      return getTaint(arrElements[0]);
+    } else {
+      return t.BinaryExpression(
+        '|',
+        chainBinaryOr(arrElements.slice(0, arrElements.length - 1)),
+        chainBinaryOr(arrElements.slice(arrElements.length - 1))
+      );
+    }
+  }
+
+  /**
+   * getTaint - returns the taint value of node
+   *
+   * @param  {type} node VariableDeclarator.init
+   * @return {type}      taint expression of node
+   */
+  function getTaint(node) {
+    if (t.isIdentifier(node)) {
+      var tmId = t.identifier("taintMap");
+      var tmExpression = t.MemberExpression(
+        tmId,
+        t.identifier(node.name)
+      );
+      return tmExpression;
+    } else if (t.isLiteral(node)) {
+      return t.numericLiteral(0);
+    } else if (t.isCallExpression(node)) {
+      console.log("call expression :(");
+    } else {
+        return 0;
+    }
+  }
 
   /**
    * createTaintStatusUpdate -
    * helper function to create taintMap.<varName> = taint
    *
-   * @param  {Path} path Path object from Babel
+   * @param  {Path} path        Path object from Babel
    * @param  {String} varName
-   * @return {null} Instruments the code, no return value
+   * @return {null}             Instruments the code, no return value
    */
   function createTaintStatusUpdate(path, lhsName, rhsTaint) {
     var name = t.identifier(
@@ -41,27 +83,33 @@ module.exports = function(babel) {
   }
 
   //////////// Instrumentation functions ////////////
+  /**
+   * handleVariableDeclarator - dispatch method for VariableDeclarator
+   *
+   * @param  {AST node} varDeclarator AST type VariableDeclarator(id, init)
+   * @param  {Path} path              Babel path object
+   * @return {null}                   Instruments the code, no return value
+   */
   function handleVariableDeclarator(varDeclarator, path) {
-    // varDeclarator is of AST type VariableDeclarator(id, init)
+    var lhsName = varDeclarator.id.name;
+
+      //////////// Identifier ////////////
     if (t.isIdentifier(varDeclarator.init)) {
-      var lhsName = varDeclarator.id.name;
-      var tmId = t.identifier("taintMap");
-      var rhsTaint = t.MemberExpression(
-        tmId,
-        t.identifier(varDeclarator.init.name)
-      );
+      var rhsTaint = getTaint(varDeclarator.init);
       createTaintStatusUpdate(path, lhsName, rhsTaint);
 
+      //////////// Literal ////////////
     } else if (t.isLiteral(varDeclarator.init)) {
       // t.isLiteral is not a public method, but is a useful
       // undocumented function that tests for AST Literal nodes
-      var lhsName = varDeclarator.id.name;
-      var rhsTaint = t.numericLiteral(0)
+      var rhsTaint = getTaint(varDeclarator.init)
       createTaintStatusUpdate(path, lhsName, rhsTaint);
 
-    } else if (t.isArrayExpression) {
-
-      console.log("hola amigos!");
+      //////////// ArrayExpression ////////////
+    } else if (t.isArrayExpression(varDeclarator.init)) {
+      var arrElements = varDeclarator.init.elements;
+      var rhsTaint = chainBinaryOr(arrElements);
+      createTaintStatusUpdate(path, lhsName, rhsTaint);
     }
   }
 
--- example.js
+++ example.js
@@ -1,2 +1,3 @@
 var a = 10;
 var b = a;
+var c = [1,a,b];
