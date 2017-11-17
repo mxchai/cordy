f4f7d5c63099edb2209022a3d9452df997741994
garbanzos <yiyant@gmail.com>
2017-11-17 15:14:34 +0800

    change all the  to  so that cordova can run it

--- checkTaint.js
+++ checkTaint.js
@@ -1,11 +1,11 @@
 function checkTaint() {
-  let lineNum = arguments[0];
-  let result = 0;
-  for (let i = 1; i < arguments.length; i++) {
+  var lineNum = arguments[0];
+  var result = 0;
+  for (var i = 1; i < arguments.length; i++) {
     result |= arguments[i];
   }
 
-  if (result) {
+  if (result === 1) {
     console.log("CORDY WARNING: Line " + lineNum + " might be tainted.");
   }
 }
