1bcd7d260492bf0bf4e3d906f577b4a09985d126
garbanzos <yiyant@gmail.com>
2017-11-17 14:49:59 +0800

    commit run.js

--- run.js
+++ run.js
@@ -5,6 +5,10 @@ var cordy = require('./cordy');
 
 var inputFile = process.argv[2];
 var outputFile = 'output.js'
+var checkTaintFile = 'checkTaint.js';
+
+var checkTaintSrc = fs.readFileSync(checkTaintFile, 'utf-8');
+// console.log(checkTaintSrc.toString());
 
 fs.readFile(inputFile, 'utf-8', function(err, data) {
   var src = data.toString();
@@ -13,7 +17,8 @@ fs.readFile(inputFile, 'utf-8', function(err, data) {
     plugins: [cordy]
   });
 
-  console.log(out.code);
+  console.log(checkTaintSrc.toString() + out.code);
+  out.code = checkTaintSrc.toString() + out.code;
   fs.writeFile(outputFile, out.code, function(err, data) {
     if (err) {
       console.log(err);
